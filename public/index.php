b8b6c7a2bcf1793f0a5dc4fbee23f46f3f37bb96
John Q Developer <202678382+JohnQDeveloper@users.noreply.github.com>
2025-07-28 08:08:15 -0400

    workable baseline

---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+
+    echo "Hello World! This is the MultiverseIdle.com server.\n";
+    echo "This is a placeholder for the main entry point of the application.\n";
--- run-local.sh
+++ run-local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source env.sh
 
-docker build -t MultiverseIdle:local .
+sudo docker build -t multiverse-idle:local .
 
-docker run -v $PWD:/app -p 8383:80 -p 4444:443 -p 443:443/udp --tty -it --rm \
---name MultiverseIdle-Local MultiverseIdle:local
+sudo docker run -v $PWD:/app -p 8383:80 -p 4444:443 -p 443:443/udp --tty -it --rm \
+--name multiverse-idle-local multiverse-idle:local
