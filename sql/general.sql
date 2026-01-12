c4d1b548878f8571e765559134a4c83cc18b36d6
JQD <202678382+JohnQDeveloper@users.noreply.github.com>
2026-01-12 12:05:39 -0500

    Initial table layout

---
+++ sql/general.sql
@@ -0,0 +1,42 @@
+/* MAIN CHARACTERS TABLE */
+CREATE TABLE
+  `characters` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `last_save` timestamp NULL DEFAULT NULL,
+    `name` varchar(255) DEFAULT NULL,
+    `level` bigint unsigned DEFAULT NULL,
+    `arena_floor` bigint unsigned DEFAULT NULL,
+    `gold` bigint unsigned DEFAULT NULL,
+    `iron` bigint unsigned DEFAULT NULL,
+    `herbs` bigint unsigned DEFAULT NULL,
+    `gems` bigint unsigned DEFAULT NULL,
+    `party_json` json DEFAULT NULL,
+    `worker_json` json DEFAULT NULL,
+    `rift_queued` bigint unsigned DEFAULT NULL,
+    `world_boss_queued` int DEFAULT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
+
+/* RIFT QUEUE TABLE */
+CREATE TABLE
+  `rifts` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `owner_id` int DEFAULT NULL,
+    `details` json DEFAULT NULL,
+    `queue_position` int DEFAULT NULL,
+    `market_price` bigint unsigned DEFAULT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
+
+/* GEAR TABLE */
+CREATE TABLE
+  `gear` (
+    `id` int unsigned NOT NULL AUTO_INCREMENT,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `details` json DEFAULT NULL,
+    `market_price` bigint unsigned DEFAULT NULL,
+    `owner_id` bigint unsigned DEFAULT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci
