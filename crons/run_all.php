f339c993b86612bcee7af5b123376d94afdd1207
JQD <202678382+JohnQDeveloper@users.noreply.github.com>
2026-01-21 08:52:28 -0500

    Run all crons with execution time in logs

---
+++ crons/run_all.php
@@ -0,0 +1,18 @@
+#!/usr/local/bin/php
+<?php
+    $run_all_time_start = microtime(true);
+
+    // Run all crons
+    echo "Starting Workers Cron...\n";
+    require_once('workers.php');
+    echo "Workers Cron Completed.\n";
+
+    echo "Starting Arena Cron...\n";
+    require_once('arena.php');
+    echo "Arena Cron Completed.\n";
+
+    echo "All Crons Completed.\n";
+
+    $run_all_time_end = microtime(true);
+    $run_all_execution_time = ($run_all_time_end - $run_all_time_start);
+    echo "Total Crons execution time: ".$run_all_execution_time." seconds\n";
